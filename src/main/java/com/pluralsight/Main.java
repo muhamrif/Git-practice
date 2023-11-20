bde49294ff1d7698129963048d7e78bcb07c29a4
akhilsomaiya07 <akhilsomaiya7@gmail.com>
2023-11-20 16:05:44 -0500

    made changes 3

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,11 @@
 package com.pluralsight;
 
 public class Main {
-
+    public void run (){
+        System.out.println("I am running");
+    }
     public static void main(String[] args) {
+
 
     }
 }
